8b9e16386ef3c875d50038792b542cc7db1d9bbf
00phorak <00phorak@gmail.com>
2025-02-05 21:41:43 +0100

    functional udp with build

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,16 @@
 const std = @import("std");
+const net = std.net;
+const posix = std.posix;
 
-pub fn main() !void {}
+pub fn main() !void {
+    const addr = try net.Address.parseIp("127.0.0.1", 2048);
+    const socket = try posix.socket(posix.AF.INET, posix.SOCK.DGRAM, posix.IPPROTO.UDP);
+    defer posix.close(socket);
+
+    try posix.bind(socket, &addr.any, addr.getOsSockLen());
+
+    var buf: [1024]u8 = undefined;
+
+    const len = try posix.recv(socket, &buf, 0);
+    std.debug.print("{s}\n", .{buf[0..len]});
+}
