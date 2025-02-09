4ac51a11f327f087b86d0e825a6fa894c7e22548
= <=>
2025-02-09 13:20:20 +0100

    add docs and make fns public

--- src/buffer.zig
+++ src/buffer.zig
@@ -9,6 +9,7 @@ pub const BytePacketBuffer = struct {
     buf: [512]u8,
     pos: usize,
 
+    /// Init new BytePacketBuffer object
     pub fn new() BytePacketBuffer {
         return BytePacketBuffer{
             .buf = undefined,
@@ -16,16 +17,18 @@ pub const BytePacketBuffer = struct {
         };
     }
 
-    fn step(self: BytePacketBuffer, steps: usize) !void {
+    /// Modify position by stepping over by number of steps
+    pub fn step(self: BytePacketBuffer, steps: usize) !void {
         self.pos += steps;
     }
 
-    fn seek(self: BytePacketBuffer, pos: usize) !void {
+    /// Set position to given position
+    pub fn seek(self: BytePacketBuffer, pos: usize) !void {
         self.pos = pos;
     }
 
-    // Read a single byte and move position forward
-    fn read(self: BytePacketBuffer) BytePacketBufferError!u8 {
+    /// Read a single byte and move position forward
+    pub fn read(self: BytePacketBuffer) BytePacketBufferError!u8 {
         if (self.pos >= 512) {
             return BytePacketBufferError.EndOfBuffer;
         }
@@ -34,30 +37,31 @@ pub const BytePacketBuffer = struct {
         return result;
     }
 
-    fn get(self: BytePacketBuffer, pos: usize) BytePacketBufferError!u8 {
+    /// Get byte at current position
+    pub fn get(self: BytePacketBuffer, pos: usize) BytePacketBufferError!u8 {
         if (pos >= 512) {
             return BytePacketBufferError.EndOfBuffer;
         }
         return self.buf[pos];
     }
 
-    // Read a range of bytes, does not modify position
-    fn getRange(self: BytePacketBuffer, start: usize, len: usize) BytePacketBufferError!u8 {
+    /// Read a range of bytes, does not modify position
+    pub fn getRange(self: BytePacketBuffer, start: usize, len: usize) BytePacketBufferError!u8 {
         if (start + len >= 512) {
             return BytePacketBufferError.EndOfBuffer;
         }
         return self.buf[start..(start + len)];
     }
 
-    // Read two bytes, step two steps forward
-    fn readU16(self: BytePacketBuffer) u16 {
+    /// Read two bytes, step two steps forward
+    pub fn readU16(self: BytePacketBuffer) u16 {
         const result = (@as(u16, self.read()) << 8) | (@as(u16, self.read()));
 
         return result;
     }
 
-    // Read four bytes, step four steps forward
-    fn readU32(self: BytePacketBuffer) u32 {
+    /// Read four bytes, step four steps forward
+    pub fn readU32(self: BytePacketBuffer) u32 {
         const result = (@as(u32, self.read()) << 24) | (@as(u32, self.read()) << 16) | (@as(u32, self.read()) << 8) | (@as(u32, self.read()) << 0);
 
         return result;
