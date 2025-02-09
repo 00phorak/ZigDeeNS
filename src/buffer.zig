96a85bc4af2d95da834017b4a2af1f700b421da2
= <=>
2025-02-09 13:38:40 +0100

    read qname WIP

--- src/buffer.zig
+++ src/buffer.zig
@@ -3,6 +3,7 @@ const expect = std.testing.expect;
 
 pub const BytePacketBufferError = error{
     EndOfBuffer,
+    JumpsExceeded,
 };
 
 pub const BytePacketBuffer = struct {
@@ -65,6 +66,60 @@ pub const BytePacketBuffer = struct {
         const result = (@as(u32, self.read()) << 24) | (@as(u32, self.read()) << 16) | (@as(u32, self.read()) << 8) | (@as(u32, self.read()) << 0);
 
         return result;
+    }
+
+    /// TODO: describe and fix working with strings... then make public
+    fn readQName(self: BytePacketBuffer, outstr: []u8) BytePacketBufferError!void {
+        var currPos = self.pos;
+
+        var jumped = false;
+        const maxJumps = 5;
+        var currJumps = 0;
+
+        var delim = "";
+
+        while (true) {
+            if (currJumps > maxJumps) {
+                return BytePacketBufferError.JumpsExceeded;
+            }
+
+            const len = self.get(currPos);
+
+            if ((len & 0xC0) == 0xC0) {
+                if (!jumped) {
+                    self.seek(currPos + 2);
+                }
+
+                const b2 = @as(u16, self.get(currPos + 1));
+
+                const offset = ((@as(u16, len) ^ 0xc0) << 8) | b2;
+                currPos = @as(usize, offset);
+
+                jumped = true;
+                currJumps += 1;
+                continue;
+            } else {
+                currPos += 1;
+
+                if (len == 0) {
+                    break;
+                }
+
+                // TODO: Use allocator..... damn strings in zig lmao
+                outstr = outstr ++ delim;
+
+                const strBuffer = self.getRange(currPos, @as(usize, len));
+                outstr = outstr ++ strBuffer;
+
+                delim = ".";
+
+                currPos += @as(usize, len);
+            }
+        }
+
+        if (!jumped) {
+            self.seek(currPos);
+        }
     }
 };
 
