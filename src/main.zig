8bf95f3c0a28fa993814fd573980bd209a1ab614
= <=>
2025-02-09 12:00:58 +0100

    packet_buffer wip

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
+const expect = std.testing.expect;
 const net = std.net;
 const posix = std.posix;
+const packet_buffer = @import("packet_buffer.zig");
 
 pub fn main() !void {
     const addr = try net.Address.parseIp("127.0.0.1", 2048);
@@ -9,18 +11,44 @@ pub fn main() !void {
 
     try posix.bind(socket, &addr.any, addr.getOsSockLen());
 
-    var buf: [1024]u8 = undefined;
-
+    var bytePacketBuffer: packet_buffer.BytePacketBuffer = packet_buffer.BytePacketBuffer.new();
     // TODO: [1] trim and compare
     const exit = "exit\r\n";
 
     while (true) {
-        const len = try posix.recvfrom(socket, &buf, 0, null, null);
-        const input = buf[0..len];
+        // recv calls recvfrom anyways..
+        const len = try posix.recvfrom(socket, &bytePacketBuffer.buf, 0, null, null);
+        const input = bytePacketBuffer.buf[0..len];
         // TODO: [1] trim and compare
         if (std.mem.eql(u8, input, exit)) {
             return;
         }
-        std.debug.print("{s}\n", .{buf[0..len]});
+        std.debug.print("{s}\n", .{bytePacketBuffer.buf[0..len]});
     }
 }
+
+// TODO: move Response code to separate file
+test "dns response code enum" {
+    try expect(@as(ResponseCode, @enumFromInt(0)) == ResponseCode.NOERROR);
+    try expect(@as(ResponseCode, @enumFromInt(1)) == ResponseCode.FORMERR);
+    try expect(@as(ResponseCode, @enumFromInt(2)) == ResponseCode.SERVFAIL);
+    try expect(@as(ResponseCode, @enumFromInt(3)) == ResponseCode.NXDOMAIN);
+    try expect(@as(ResponseCode, @enumFromInt(4)) == ResponseCode.NOTIMP);
+    try expect(@as(ResponseCode, @enumFromInt(5)) == ResponseCode.REFUSED);
+    // safety check for exhaustive enum
+    const T = @TypeOf(@as(ResponseCode, @enumFromInt(6)));
+    try expect(T == ResponseCode);
+}
+
+test "dns response code switch logic" {
+    const testValue: ResponseCode = @enumFromInt(6);
+    const result = switch (testValue) {
+        .NOERROR, .FORMERR, .SERVFAIL, .NXDOMAIN, .REFUSED => false,
+        else => true,
+    };
+    try expect(result);
+}
+
+const ResponseCode = enum(u3) { NOERROR, FORMERR, SERVFAIL, NXDOMAIN, NOTIMP, REFUSED, _ };
+
+const Header = struct { packet_identifier: u16, query_response: u1, operation_code: u4, authoritative_answer: u1, truncated_message: u1, recursion_desired: u1, recursion_available: u1, reserved: u3, response_code: ResponseCode, question_count: u16, answer_count: u16, authority_count: u16, additional_count: u16 };
---
+++ src/packet_buffer.zig
@@ -0,0 +1,74 @@
+const std = @import("std");
+const expect = std.testing.expect;
+
+pub const BytePacketBufferError = error{
+    EndOfBuffer,
+};
+
+pub const BytePacketBuffer = struct {
+    buf: [512]u8,
+    pos: usize,
+
+    pub fn new() BytePacketBuffer {
+        return BytePacketBuffer{
+            .buf = undefined,
+            .pos = 0,
+        };
+    }
+
+    fn getPos(self: BytePacketBuffer) usize {
+        return self.pos;
+    }
+
+    fn step(self: BytePacketBuffer, steps: usize) !void {
+        self.pos += steps;
+    }
+
+    fn seek(self: BytePacketBuffer, pos: usize) !void {
+        self.pos = pos;
+    }
+
+    // Read a single byte and move position forward
+    fn read(self: BytePacketBuffer) BytePacketBufferError!u8 {
+        if (self.pos >= 512) {
+            return BytePacketBufferError.EndOfBuffer;
+        }
+        const result = self.buf[self.pos];
+        self.pos += 1;
+        return result;
+    }
+
+    fn get(self: BytePacketBuffer, pos: usize) BytePacketBufferError!u8 {
+        if (pos >= 512) {
+            return BytePacketBufferError.EndOfBuffer;
+        }
+        return self.buf[pos];
+    }
+
+    // Read a range of bytes, does not modify position
+    fn getRange(self: BytePacketBuffer, start: usize, len: usize) BytePacketBufferError!u8 {
+        if (start + len >= 512) {
+            return BytePacketBufferError.EndOfBuffer;
+        }
+        return self.buf[start..(start + len)];
+    }
+
+    // Read two bytes, step two steps forward
+    fn readU16(self: BytePacketBuffer) u16 {
+        const result = (@as(u16, self.read()) << 8) | (@as(u16, self.read()));
+
+        return result;
+    }
+
+    // Read four bytes, step four steps forward
+    fn readU32(self: BytePacketBuffer) u32 {
+        const result = (@as(u32, self.read()) << 24) | (@as(u32, self.read()) << 16) | (@as(u32, self.read()) << 8) | (@as(u32, self.read()) << 0);
+
+        return result;
+    }
+};
+
+test "bytepacketbuffer init" {
+    const buffer: BytePacketBuffer = BytePacketBuffer.new();
+    try expect(512 == buffer.buf.len);
+}
